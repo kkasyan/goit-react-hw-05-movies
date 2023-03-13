1a9cbbc1b6da811814901842412c4e782bc1685c
kkasyan <104075234+kkasyan@users.noreply.github.com>
2023-03-13 21:34:38 +0200

    edited axios requests

--- src/shared/api/movies.js
+++ src/shared/api/movies.js
@@ -1,35 +1,36 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+const lang = 'en-US';
+const page = '1';
 const KEY = 'd0cbba0e8b5863a2012bf39c965ec970';
 
+axios.defaults.params = { api_key: KEY, landuage: lang, page: page };
+
 export const getTrendingMovies = async () => {
-  const response = await axios.get(`trending/all/day?api_key=${KEY}`);
+  const response = await axios.get(`trending/all/day?`);
   return response.data;
 };
 
 export const getMovieDetails = async id => {
-  const response = await axios.get(`movie/${id}?api_key=${KEY}&language=en-US`);
+  const response = await axios.get(`movie/${id}?`);
   return response.data;
 };
 
 export const getMovieReviews = async id => {
-  const response = await axios.get(
-    `movie/${id}/reviews?api_key=${KEY}&language=en-US&page=1`
-  );
+  const response = await axios.get(`movie/${id}/reviews?`);
   return response.data;
 };
 
 export const getMovieCast = async id => {
-  const response = await axios.get(
-    `movie/${id}/credits?api_key=${KEY}&language=en-US`
-  );
+  const response = await axios.get(`movie/${id}/credits?`);
   return response.data;
 };
 
 export const searchMovie = async q => {
   const response = await axios.get(
-    `search/movie?api_key=${KEY}&language=en-US&page=1&include_adult=false&query=${q}`
+    `search/movie?&include_adult=false&query=${q}`
   );
   return response.data;
 };
